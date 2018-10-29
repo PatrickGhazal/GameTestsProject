84fadffb099feaef42606b7998e1bbd1e52055d8
PatrickGhazal <patrick.ghazal@mail.mcgill.ca>
2018-10-28 20:10:49 -0400

    Name updates

---
+++ GameClassTests.java
@@ -0,0 +1,11 @@
+package org.scoutant.blokish.model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ GameClassWhiteBoxTesting.class, GameClassBlackBoxTesting.class })
+public class GameClassTests {
+
+}
